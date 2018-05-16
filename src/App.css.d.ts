6e8e94ba55e9118be8454c8db056a08789c1927c
Wee <fi3ework@gmail.com>
2018-05-16 22:54:33 +0800

    feat(webpack): add sacc, postcss, eslint, vscode setting, mobile adaption

---
+++ src/App.css.d.ts
@@ -0,0 +1,7 @@
+export const App: string;
+export const app: string;
+export const appLogo: string;
+export const appLogoSpin: string;
+export const appHeader: string;
+export const appTitle: string;
+export const appIntro: string;
